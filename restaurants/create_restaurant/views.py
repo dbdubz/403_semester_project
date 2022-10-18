be3d09cd9bcc57c93e368aa8ff7e5ed9fabf8ae6
Dalyn Baldwin <dlbaldwi@byu.edu>
2022-10-17 21:39:40 -0600

    set views and paths for create app

--- restaurants/create_restaurant/views.py
+++ restaurants/create_restaurant/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def add_form(request):
+    return HttpResponse('Form for restaurant add!')
+def create(request, name, address, phone):
+    output = f'Added {name} at {address}. Contact them at: {phone}.'
+    return HttpResponse(output)

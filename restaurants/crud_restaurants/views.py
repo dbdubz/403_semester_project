18aadc9a7ee581ecccbd82f1902d18966d76f3f3
Dalyn Baldwin <dlbaldwi@byu.edu>
2022-11-15 02:26:43 -0700

    Ready for templates

--- restaurants/crud_restaurants/views.py
+++ restaurants/crud_restaurants/views.py
@@ -3,27 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 def add_form(req):
-    return HttpResponse('Form for restaurant add!')
+    return render(req, 'restaurants/add.html')
 def create_restaurant(req, name, address, phone):
     output = f'Added {name} at {address}. Contact them at: {phone}.'
     return HttpResponse(output)
 def delete_form(req):
-    return HttpResponse('Form for restaurant delete!')
+    return render(req, 'restaurants/delete.html')
 def delete_restaurant(req, restaurantID):
     output = f'Deleted restaurant number {restaurantID}.'
     return HttpResponse(output)
 def restaurants_index(req):
-    return HttpResponse('Index (READ) Page!')
-def about_restaurant(req, restaurantID):
+    return render(req, 'restaurants/restaurants.html')
+def restaurant_info(req, restaurantID):
     output = f'Viewing page for restaurant {restaurantID}'
-    return HttpResponse(output)
+    return 
 def update_form(req):
-    return HttpResponse('Update Restaurant Page!')
+    return render(req, 'restaurants/update.html')
 def update_restaurant(req, restaurantID):
     output = f'Update form for restaurant {restaurantID}'
     return HttpResponse(output)
 def landing_page(req):
-    return HttpResponse('Main Home Landing Page!')
+    return render(req, 'restaurants/index.html')
 def about_page(req):
-    output = f'About Page'
-    return HttpResponse(output)
+    return render(req, 'restaurants/about.html')
--- restaurants/restaurants/settings.py
+++ restaurants/restaurants/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'restaurants.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
